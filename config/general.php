10905108512374e056cdd4c60e8946298ef8a0e1
Angelo Keirsebilck <angelokeirsebilck@Mac-Pro-van-Angelo.local>
2022-08-13 07:44:27 +0200

    Add pro version of craft + change general config settings

--- config/general.php
+++ config/general.php
@@ -15,14 +15,38 @@ $isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
 $isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
 
 return GeneralConfig::create()
+
+->aliases([
+    '@web' => craft\helpers\App::env('PRIMARY_SITE_URL'),
+    '@webroot' => dirname(__DIR__) . '/web',
+])
+
     // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(1)
     // Prevent generated URLs from including "index.php"
     ->omitScriptNameInUrls()
     // Enable Dev Mode on the dev environment (see https://craftcms.com/guides/what-dev-mode-does)
     ->devMode($isDev)
-    // Only allow administrative changes on the dev environment
-    ->allowAdminChanges($isDev)
+
+    ->allowUpdates(App::env('ALLOW_UPDATES'))
+
+    ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES'))
+
     // Disallow robots everywhere except the production environment
     ->disallowRobots(!$isProd)
+
+    ->extraFileKinds([
+        'svg' => [
+            'label' => 'SVG',
+            'extensions' => ['svg'],
+        ],
+        'mp4' => [
+            'label' => 'MP4',
+            'extensions' => ['mp4'],
+        ],
+    ])
+
+    ->enableGql(true)
+
+    ->maxUploadFileSize('50M')
 ;
