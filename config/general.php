5cf050e17c62ebdea3f78e1e54470057df24fe80
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-08-19 09:50:47 +0200

    Add Vize pagebuilder

--- config/general.php
+++ config/general.php
@@ -18,6 +18,7 @@ return GeneralConfig::create()
 
 ->aliases([
     '@web' => craft\helpers\App::env('PRIMARY_SITE_URL'),
+    '@assetsUrl' => craft\helpers\App::env('ASSETS_URL'),
     '@previewUrl' => craft\helpers\App::env('PREVIEW_URL'),
     '@webroot' => dirname(__DIR__) . '/web',
 ])
